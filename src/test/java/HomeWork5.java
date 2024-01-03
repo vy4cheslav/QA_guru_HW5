5d4489533fe1b40e5c9ee9e7aac6a1beb749cca8
vy4cheslav <d4m1enpwnz@gmail.com>
2024-01-03 17:32:23 +0300

    Ready

--- src/test/java/HomeWork5.java
+++ src/test/java/HomeWork5.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class HW5 {
+public class HomeWork5 {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
