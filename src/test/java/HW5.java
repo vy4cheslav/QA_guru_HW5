939cd2b101d9e25a0249074333eeccb11838f32b
vy4cheslav <d4m1enpwnz@gmail.com>
2024-01-03 17:31:19 +0300

    Ready

---
+++ src/test/java/HW5.java
@@ -0,0 +1,40 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class HW5 {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void firstTest() {
+        open("selenide/selenide");  //Откройте страницу Selenide в Github
+        $(By.id("wiki-tab")).click();  //Перейдите в раздел Wiki проекта
+        $(byText("Soft assertions")).shouldBe(visible).click();//Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $(By.id("repo-content-turbo-frame")).shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}")).shouldBe(visible);//Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+
+    }
+}
+
